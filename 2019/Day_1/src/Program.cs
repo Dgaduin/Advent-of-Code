18294552e59b6efbd2ff1a2bab7aa4cf0e649ce7
Atanas Pashkov <atanas.pashkov@globalreachgroup.com>
2019-12-02 09:53:31 +0000

    Complete Day 1 2019

---
+++ 2019/Day_1/src/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Day_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = File.ReadLines("input.txt").ToList();
+            // string input = File.ReadAllText("input.txt");
+
+            Console.WriteLine(input.Select(x => Task1(Int32.Parse(x))).Sum());
+            Console.WriteLine(input.Select(x => Task2(Int32.Parse(x))).Sum());
+        }
+
+        public static int Task1(int value) => value / 3 - 2;
+
+        public static int Task2(int value, int fuelTotal = 0)
+        {
+            var fuel = Task1(value);
+            if (fuel <= 8) return fuel + fuelTotal;
+            else return Task2(fuel, fuel + fuelTotal);
+        }
+    }
+}
---
+++ 2019/Day_1/test/Tests.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Day_1.Test
+{
+    public class Test
+    {
+        [Fact]
+        public void Task1_1()
+        {
+            Assert.Equal(2, Program.Task1(12));
+        }
+
+        [Fact]
+        public void Task1_2()
+        {
+            Assert.Equal(2, Program.Task1(14));
+        }
+
+        [Fact]
+        public void Task1_3()
+        {
+            Assert.Equal(654, Program.Task1(1969));
+        }
+
+        [Fact]
+        public void Task1_4()
+        {
+            Assert.Equal(33583, Program.Task1(100756));
+        }
+
+        [Fact]
+        public void Task2_1()
+        {
+            Assert.Equal(2, Program.Task2(14));
+        }
+
+        [Fact]
+        public void Task2_2()
+        {
+            Assert.Equal(966, Program.Task2(1969));
+        }
+
+        [Fact]
+        public void Task2_3()
+        {
+            Assert.Equal(50346, Program.Task2(100756));
+        }
+    }
+}
