dff24878dcae1cfbe226b9f3f82bae4fd0c27889
Atanas Pashkov <atanas.pashkov@globalreachgroup.com>
2019-12-02 14:46:27 +0000

    Complete Day 6 2015

--- 2015/Day_06/src/Program.cs
+++ 2015/Day_06/src/Program.cs
@@ -13,8 +13,8 @@ namespace Day_06
             List<string> input = File.ReadLines("input.txt").ToList();
             // string input = File.ReadAllText("input.txt");
 
-            Console.WriteLine(Task1());
-            Console.WriteLine(Task2());
+            Console.WriteLine(Task1(input));
+            Console.WriteLine(Task2(input));
         }
 
         public static string Task1(List<string> input)
@@ -64,6 +64,54 @@ namespace Day_06
 
             return count.ToString();
         }
-        public static string Task2() { return ""; }
+        public static string Task2(List<string> input)
+        {
+            int[,] lights = new int[1000, 1000];
+
+            var r1 = new Regex("(toggle|on|off) (\\d{1,}),(\\d{1,}).+ (\\d{1,}),(\\d{1,})");
+            foreach (string s in input)
+            {
+                var match = r1.Match(s).Groups;
+                var type = match[1].Value switch
+                {
+                    "toggle" => 1,
+                    "on" => 2,
+                    "off" => 3,
+                    _ => 0
+                };
+
+                for (int i = Int32.Parse(match[2].Value); i <= Int32.Parse(match[4].Value); i++)
+                {
+                    for (int j = Int32.Parse(match[3].Value); j <= Int32.Parse(match[5].Value); j++)
+                    {
+                        switch (type)
+                        {
+                            case 1:
+                                lights[i, j] += 2;
+                                break;
+                            case 2:
+                                lights[i, j]++;
+                                break;
+                            case 3:
+                                lights[i, j]--;
+                                if (lights[i, j] < 0)
+                                    lights[i, j] = 0;
+                                break;
+                        }
+                    }
+                }
+            }
+
+            int count = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                for (int j = 0; j < 1000; j++)
+                {
+                    count = count + lights[i, j];
+                }
+            }
+
+            return count.ToString();
+        }
     }
 }
