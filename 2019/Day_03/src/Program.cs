48c0f579c446a6aa2f6414b7ae2c880ecb457301
Atanas Pashkov <atanas.pashkov@globalreachgroup.com>
2019-12-03 12:10:55 +0000

    Complete Day 3 2019 Task 1

---
+++ 2019/Day_03/src/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Day_03
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = File.ReadLines("input.txt").ToList();
+
+            Console.WriteLine(Task1(input));
+            Console.WriteLine(Task2());
+        }
+
+        static HashSet<(int x, int y)> GenerateWirePath(string value)
+        {
+            var values = value.Split(',');
+            (int x, int y) position = (0, 0);
+            var paths = new HashSet<(int x, int y)>();
+            foreach (var wire in values)
+            {
+                var direction = wire.Substring(0, 1);
+                var distance = Int32.Parse(wire.Substring(1));
+                IEnumerable<(int x, int y)> newPositions = null;
+                switch (direction)
+                {
+                    case "R":
+                        {
+                            newPositions = Enumerable.Range(position.x, distance).Select(w => (x: w, y: position.y));
+                            position.x += distance;
+                            break;
+                        }
+                    case "L":
+                        {
+                            newPositions = Enumerable.Range(position.x - distance, distance).Select(w => (x: w, y: position.y));
+                            position.x -= distance;
+                            break;
+                        }
+                    case "U":
+                        {
+                            newPositions = Enumerable.Range(position.y, distance).Select(w => (x: position.x, y: w));
+                            position.y += distance;
+                            break;
+                        }
+                    case "D":
+                        {
+                            newPositions = Enumerable.Range(position.y - distance, distance).Select(w => (x: position.x, y: w));
+                            position.y -= distance;
+                            break;
+                        }
+                };
+                paths.UnionWith(newPositions);
+            }
+            paths.Remove((0, 0));
+            return paths;
+        }
+
+        public static string Task1(List<string> input)
+        {
+            var wire1 = GenerateWirePath(input[0]);
+            var wire2 = GenerateWirePath(input[1]);
+
+            return wire1
+                .Intersect(wire2)
+                .Select(x => Math.Abs(x.x) + Math.Abs(x.y))
+                .Min()
+                .ToString();
+        }
+        public static string Task2() { return ""; }
+    }
+}
---
+++ 2019/Day_03/test/Tests.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Day_03.Test
+{
+    public class Test
+    {
+        [Fact]
+        public void Task1_1()
+        {
+            Assert.Equal("159", Program.Task1(new List<string> { "R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83" }));
+        }
+
+        [Fact]
+        public void Task1_2()
+        {
+            Assert.Equal("135", Program.Task1(new List<string> { "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7" }));
+        }
+
+        [Fact]
+        public void Task2_1()
+        {
+            Assert.Equal("", Program.Task2());
+        }
+    }
+}
