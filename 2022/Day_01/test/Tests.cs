c2f7e8a46653149913a726dec50f212e321410cc
Atanas Pashkov <dgaduin@gmail.com>
2022-12-01 14:08:36 +0000

    Setup test 1

--- 2022/Day_01/test/Tests.cs
+++ 2022/Day_01/test/Tests.cs
@@ -6,10 +6,30 @@ namespace Day_01.Test
 {
     public class Test
     {
+        public List<List<int>> Elfs;
+        public Test()
+        {
+            var input = new List<string>{
+                "1000",
+                "2000",
+                "3000",
+                "",
+                "4000",
+                "",
+                "5000",
+                "6000",
+                "",
+                "7000",
+                "8000",
+                "9000",
+                "",
+                "10000"
+            };
+        }
         [Fact]
         public void Task1_1()
         {
-            Assert.Equal("", Program.Task1());
+            Assert.Equal(24000, Program.Task1(Elfs));
         }
 
         [Fact]
