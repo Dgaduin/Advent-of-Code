95dd68a13dcf03e15e69a80f8990c5186ed82ed3
Atanas Pashkov <dgaduin@gmail.com>
2022-12-01 13:18:37 +0000

    Prep for Day1

---
+++ 2022/Day_01/README.md
@@ -0,0 +1,6 @@
+# Template for Advent of Code in `C#`
+
+This includes baseline source file with options to read line by line or the full text
+
+Install by `dotnet new --install path\to\template-folder`
+Use like `dotnet new aoc -n Day_X`
---
+++ 2022/Day_01/test/Tests.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Day_01.Test
+{
+    public class Test
+    {
+        [Fact]
+        public void Task1_1()
+        {
+            Assert.Equal("", Program.Task1());
+        }
+
+        [Fact]
+        public void Task2_1()
+        {
+            Assert.Equal("", Program.Task2());
+        }
+    }
+}
