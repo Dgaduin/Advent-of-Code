8181774c4be9debb78e1a7c85dba79b312946dfd
Atanas Pashkov <dgaduin@gmail.com>
2022-12-01 14:16:07 +0000

    Use file scope namespace

--- 2022/Day_01/src/Program.cs
+++ 2022/Day_01/src/Program.cs
@@ -4,38 +4,37 @@ using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-namespace Day_01
+namespace Day_01;
+
+public class Program
 {
-    public class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        List<string> input = File.ReadLines("input.txt").ToList();
+        // string input = File.ReadAllText("input.txt");
+        var elfs = ChunkIntoElfs(input).ToList();
+
+        Console.WriteLine(Task1(elfs));
+        Console.WriteLine(Task2(elfs));
+    }
+
+    public static int Task1(List<List<int>> input) => input.Select(x => x.Sum()).Max();
+    public static int Task2(List<List<int>> input) => input.Select(x => x.Sum()).OrderByDescending(x => x).Take(3).Sum();
+
+    public static IEnumerable<List<int>> ChunkIntoElfs(List<string> input)
+    {
+        var temp = new List<int>();
+        foreach (var row in input)
         {
-            List<string> input = File.ReadLines("input.txt").ToList();
-            // string input = File.ReadAllText("input.txt");
-            var elfs = ChunkIntoElfs(input).ToList();
-
-            Console.WriteLine(Task1(elfs));
-            Console.WriteLine(Task2(elfs));
-        }
-
-        public static int Task1(List<List<int>> input) => input.Select(x => x.Sum()).Max();
-        public static int Task2(List<List<int>> input) => input.Select(x => x.Sum()).OrderByDescending(x => x).Take(3).Sum();
-
-        public static IEnumerable<List<int>> ChunkIntoElfs(List<string> input)
-        {
-            var temp = new List<int>();
-            foreach (var row in input)
+            if (row != "")
+                temp.Add(Int32.Parse(row));
+            else
             {
-                if (row != "")
-                    temp.Add(Int32.Parse(row));
-                else
-                {
-                    yield return temp;
-                    temp = new List<int>();
-                }
+                yield return temp;
+                temp = new List<int>();
             }
-            yield return temp;
-            yield break;
         }
+        yield return temp;
+        yield break;
     }
 }
--- 2022/Day_01/test/Tests.cs
+++ 2022/Day_01/test/Tests.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using Xunit;
 using System.Linq;
 
-namespace Day_01.Test
+namespace Day_01.Test;
+public class Test
 {
-    public class Test
+    public List<List<int>> Elfs;
+    public Test()
     {
-        public List<List<int>> Elfs;
-        public Test()
-        {
-            var input = new List<string>{
+        var input = new List<string>{
                 "1000",
                 "2000",
                 "3000",
@@ -26,18 +25,17 @@ namespace Day_01.Test
                 "",
                 "10000"
             };
-            Elfs = Program.ChunkIntoElfs(input).ToList();
-        }
-        [Fact]
-        public void Task1_1()
-        {
-            Assert.Equal(24000, Program.Task1(Elfs));
-        }
+        Elfs = Program.ChunkIntoElfs(input).ToList();
+    }
+    [Fact]
+    public void Task1_1()
+    {
+        Assert.Equal(24000, Program.Task1(Elfs));
+    }
 
-        [Fact]
-        public void Task2_1()
-        {
-            Assert.Equal(45000, Program.Task2(Elfs));
-        }
+    [Fact]
+    public void Task2_1()
+    {
+        Assert.Equal(45000, Program.Task2(Elfs));
     }
 }
