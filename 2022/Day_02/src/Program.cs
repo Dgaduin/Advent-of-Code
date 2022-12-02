16016cbf8db27bffe683f8d161528cb71dd16619
Dgaduin <dgaduin@gmail.com>
2022-12-02 11:10:25 +0000

    Setup Task 1

--- 2022/Day_02/src/Program.cs
+++ 2022/Day_02/src/Program.cs
@@ -9,13 +9,23 @@ public class Program
 {
     static void Main(string[] args)
     {
-        // List<string> input = File.ReadLines("input.txt").ToList();
+        var input = File.ReadLines("input.txt").ToList();
+        var strategy = GetShapes(input);
         // string input = File.ReadAllText("input.txt");
 
-        Console.WriteLine(Task1());
+        Console.WriteLine(Task1(strategy));
         Console.WriteLine(Task2());
     }
 
-    public static string Task1() { return ""; }
+    public static int Task1(List<(int, int)> input) { return 0; }
     public static string Task2() { return ""; }
+
+    public static List<(int, int)> GetShapes(List<string> input)
+    =>
+        input.Select(x =>
+        {
+            var y = x.Split(' ');
+            return (Int32.Parse(y[0]), Int32.Parse(y[2]));
+        }
+        ).ToList();
 }
--- 2022/Day_02/test/Tests.cs
+++ 2022/Day_02/test/Tests.cs
@@ -1,15 +1,28 @@
 using System;
 using System.Collections.Generic;
 using Xunit;
+using System.Linq;
 
 namespace Day_02.Test;
 
 public class Test
 {
+    public List<(int, int)> payload;
+
+    public Test()
+    {
+        payload = new List<(int, int)>
+        {
+            (1, 2),
+            (2, 1),
+            (3, 3),
+        };
+    }
+
     [Fact]
     public void Task1_1()
     {
-        Assert.Equal("", Program.Task1());
+        Assert.Equal(15, Program.Task1(payload));
     }
 
     [Fact]
