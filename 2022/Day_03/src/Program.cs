d36b47ac17fe59fdf1f0517c088ad55929453830
Dgaduin <dgaduin@gmail.com>
2022-12-03 19:14:39 +0000

    Wrap up day 3 task 1

--- 2022/Day_03/src/Program.cs
+++ 2022/Day_03/src/Program.cs
@@ -9,13 +9,30 @@ public static class Program
 {
     static void Main(string[] args)
     {
-        // List<string> input = File.ReadLines("input.txt").ToList();
+        List<string> input = File.ReadLines("input.txt").ToList();
         // string input = File.ReadAllText("input.txt");
+        var backpacks = input.ProcessBackpacks();
 
-        Console.WriteLine(Task1());
+        Console.WriteLine(Task1(backpacks));
         Console.WriteLine(Task2());
     }
 
-    public static string Task1() { return ""; }
+    public static int Task1(IEnumerable<(HashSet<char>, HashSet<char>, char common)> backpacks) => backpacks.Sum(x => x.common.CalculateScore());
+
     public static string Task2() { return ""; }
+
+    public static IEnumerable<(HashSet<char>, HashSet<char>, char)> ProcessBackpacks(this List<string> input)
+    {
+        foreach (var row in input)
+        {
+            int half = row.Length / 2;
+            var firstHalf = row.Take(half).ToHashSet();
+            var secondHalf = row.Skip(half).ToHashSet();
+            var common = firstHalf.Intersect(secondHalf).FirstOrDefault();
+            yield return (firstHalf, secondHalf, common);
+        }
+        yield break;
+    }
+
+    public static int CalculateScore(this char a) => a - 'a' < 0 ? a - 'A' + 27 : a - 'a' + 1;
 }
--- 2022/Day_03/test/Tests.cs
+++ 2022/Day_03/test/Tests.cs
@@ -6,10 +6,23 @@ namespace Day_03.Test;
 
 public class Test
 {
+    public IEnumerable<(HashSet<char>, HashSet<char>, char)> backpacks;
+    public Test()
+    {
+        var input = new List<string>(){
+            "vJrwpWtwJgWrhcsFMMfFFhFp",
+            "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+            "PmmdzqPrVvPwwTWBwg",
+            "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+            "ttgJtRGJQctTZtZT",
+            "CrZsJsPPZsGzwwsLwLmpwMDw"
+        };
+        backpacks = input.ProcessBackpacks();
+    }
     [Fact]
     public void Task1_1()
     {
-        Assert.Equal("", Program.Task1());
+        Assert.Equal(157, Program.Task1(backpacks));
     }
 
     [Fact]
