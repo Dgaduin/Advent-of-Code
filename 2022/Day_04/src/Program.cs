0e0237172f5fa2e3ff44c112b47f3285f301cbe8
Dgaduin <dgaduin@gmail.com>
2022-12-04 13:56:38 +0000

    22 day 4 task 1 done

--- 2022/Day_04/src/Program.cs
+++ 2022/Day_04/src/Program.cs
@@ -9,13 +9,24 @@ public static class Program
 {
     static void Main(string[] args)
     {
-        // List<string> input = File.ReadLines("input.txt").ToList();
+        List<string> input = File.ReadLines("input.txt").ToList();
         // string input = File.ReadAllText("input.txt");
+        var pairs = input.ReadPairs();
 
-        Console.WriteLine(Task1());
+        Console.WriteLine(Task1(pairs));
         Console.WriteLine(Task2());
     }
 
-    public static string Task1() { return ""; }
+    public static int Task1(List<List<int>> pairs) =>
+        pairs.Count(x =>
+            (x[0] <= x[2] && x[1] >= x[3]) ||
+            (x[0] >= x[2] && x[1] <= x[3]));
     public static string Task2() { return ""; }
+
+    public static List<List<int>> ReadPairs(this List<string> input) =>
+        input
+            .Select(row => row.Split(',', '-')
+                              .Select(Int32.Parse)
+                              .ToList())
+            .ToList();
 }
--- 2022/Day_04/test/Tests.cs
+++ 2022/Day_04/test/Tests.cs
@@ -7,12 +7,25 @@ namespace Day_04.Test;
 
 public class Test
 {
-    public Test() { }
+    public List<List<int>> pairs;
+    public Test()
+    {
+        var testData = new List<string>()
+        {
+            "2-4,6-8",
+            "2-3,4-5",
+            "5-7,7-9",
+            "2-8,3-7",
+            "6-6,4-6",
+            "2-6,4-8"
+        };
+        pairs = testData.ReadPairs();
+    }
 
     [Fact]
     public void Task1_1()
     {
-        Assert.Equal("", Program.Task1());
+        Assert.Equal(2, Program.Task1(pairs));
     }
 
     [Fact]
