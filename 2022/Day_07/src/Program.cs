7d3e6636c0825ee2645fe8c278ceeeac8fc5a295
Dgaduin <dgaduin@gmail.com>
2022-12-07 23:07:07 +0000

    22 07 Complete

--- 2022/Day_07/src/Program.cs
+++ 2022/Day_07/src/Program.cs
@@ -3,19 +3,105 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Diagnostics;
 
 namespace Day_07;
 public static class Program
 {
     static void Main(string[] args)
     {
-        // List<string> input = File.ReadLines("input.txt").ToList();
-        // string input = File.ReadAllText("input.txt");
+        List<string> input = File.ReadLines("input.txt").ToList();
+        var root = input.ParseInput();
 
-        Console.WriteLine(Task1());
-        Console.WriteLine(Task2());
+        Console.WriteLine(Task1(root));
+        Console.WriteLine(Task2(root));
     }
 
-    public static string Task1() { return ""; }
-    public static string Task2() { return ""; }
+    public static int Task1(Directory root)
+        => root.GetChildDirectoriesFlat().Where(x => x.Size <= 100000).Sum(x => x.Size);
+
+    public static int Task2(Directory root)
+    {
+        var remainingSize = 30000000 - (70000000 - root.Size);
+        return root.GetChildDirectoriesFlat()
+                   .Where(x => x.Size >= remainingSize)
+                   .OrderBy(x => x.Size)
+                   .First()
+                   .Size;
+    }
+
+    public static Directory ParseInput(this List<string> input)
+    {
+        var root = new Directory() { Name = "/" };
+        Directory current = root;
+        foreach (var line in input)
+        {
+            if (line[0] == '$')
+            {
+                if (line == "$ cd /" || line[2] == 'l') continue;
+                var dirName = line.Split(' ')[2];
+                if (dirName == "..") current = current.Parent;
+                else
+                {
+                    var newDir = current.GetByName(dirName);
+                    current = newDir;
+                }
+            }
+            else
+            {
+                var splitLine = line.Split(' ');
+                if (splitLine[0] == "dir")
+                {
+                    var dir = new Directory() { Name = splitLine[1], Parent = current };
+                    current.Nodes.Add(dir);
+                }
+                else
+                {
+                    var file = new NFile() { Name = splitLine[1], Size = Int32.Parse(splitLine[0]) };
+                    current.Nodes.Add(file);
+                }
+            }
+        }
+        return root;
+    }
+}
+
+public interface Node
+{
+    string Name { get; set; }
+    int Size { get; }
+}
+
+[DebuggerDisplay("- {Name} (dir, size={Size})")]
+public class Directory : Node
+{
+    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+    public Directory Parent { get; set; }
+    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+    public string Name { get; set; }
+    public List<Node> Nodes { get; set; } = new List<Node>();
+    public int Size => Nodes.Sum(x => x.Size);
+    public Directory GetByName(string name) => (Directory)Nodes.First(x => x is Directory && x.Name == name);
+
+    public List<Directory> GetChildDirectoriesFlat()
+    {
+        var list = new List<Directory>();
+        list.Add(this);
+
+        foreach (var node in Nodes)
+            if (node is Directory)
+                list.AddRange(((Directory)node).GetChildDirectoriesFlat());
+
+        return list;
+    }
+}
+
+[DebuggerDisplay("- {Name} (file, size={Size})")]
+public class NFile : Node
+{
+    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+    public string Name { get; set; }
+
+    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+    public int Size { get; set; }
 }
--- 2022/Day_07/test/Tests.cs
+++ 2022/Day_07/test/Tests.cs
@@ -2,22 +2,53 @@ using System;
 using System.Collections.Generic;
 using Xunit;
 using System.Linq;
+using Day_07;
 
 namespace Day_07.Test;
 
 public class Test
 {
-    public Test() { }
+    public Directory root;
+    public Test()
+    {
+        var input = new List<string>()
+        {
+            "$ cd /",
+            "$ ls",
+            "dir a",
+            "14848514 b.txt",
+            "8504156 c.dat",
+            "dir d",
+            "$ cd a",
+            "$ ls",
+            "dir e",
+            "29116 f",
+            "2557 g",
+            "62596 h.lst",
+            "$ cd e",
+            "$ ls",
+            "584 i",
+            "$ cd ..",
+            "$ cd ..",
+            "$ cd d",
+            "$ ls",
+            "4060174 j",
+            "8033020 d.log",
+            "5626152 d.ext",
+            "7214296 k"
+        };
+        root = input.ParseInput();
+    }
 
     [Fact]
     public void Task1_1()
     {
-        Assert.Equal("", Program.Task1());
+        Assert.Equal(95437, Program.Task1(root));
     }
 
     [Fact]
     public void Task2_1()
     {
-        Assert.Equal("", Program.Task2());
+        Assert.Equal(24933642, Program.Task2(root));
     }
 }
