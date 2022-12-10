a01cc1302bda1bee293004a5c4d8c2bd05386b84
Dgaduin <dgaduin@gmail.com>
2022-12-10 23:57:40 +0000

    22 D10 T1 T2

--- 2022/Day_10/src/Program.cs
+++ 2022/Day_10/src/Program.cs
@@ -3,19 +3,59 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Text;
 
 namespace Day_10;
 public static class Program
 {
     static void Main(string[] args)
     {
-        // List<string> input = File.ReadLines("input.txt").ToList();
+        List<string> input = File.ReadLines("input.txt").ToList();
         // string input = File.ReadAllText("input.txt");
-
-        Console.WriteLine(Task1());
-        Console.WriteLine(Task2());
+        var stack = RunProgram(input);
+        Console.WriteLine(Task1(stack));
+        Task2(stack);
     }
 
-    public static string Task1() { return ""; }
-    public static string Task2() { return ""; }
+    public static int Task1(List<int> input)
+    {
+        int sum = 0;
+        for (int i = 19; i < input.Count; i += 40)
+            sum += input[i] * (i + 1);
+        return sum;
+    }
+
+    public static void Task2(List<int> input)
+    {
+        var chunks = input.Chunk(40);
+        foreach (var chunk in chunks)
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < chunk.Length; i++)
+            {
+                if (i <= chunk[i] + 1 && i >= chunk[i] - 1)
+                    sb.Append('#');
+                else sb.Append('.');
+            }
+            Console.WriteLine(sb);
+        }
+    }
+
+    public static List<int> RunProgram(List<string> input)
+    {
+        var stack = new List<int> { 1 };
+        var commands = input.Select(x => x.Split(' '));
+        foreach (var row in commands)
+        {
+            var last = stack[^1];
+            if (row[0] == "noop")
+                stack.Add(last);
+            if (row[0] == "addx")
+            {
+                stack.Add(last);
+                stack.Add(last + Int32.Parse(row[1]));
+            }
+        }
+        return stack;
+    }
 }
