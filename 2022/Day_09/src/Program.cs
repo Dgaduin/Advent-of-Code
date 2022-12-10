d5d6081d2a6f9bf4a407949a6b87017edf1a4f0c
Dgaduin <dgaduin@gmail.com>
2022-12-10 23:17:24 +0000

    22 D9 T2

--- 2022/Day_09/src/Program.cs
+++ 2022/Day_09/src/Program.cs
@@ -12,7 +12,7 @@ public static class Program
         List<string> input = File.ReadLines("input.txt").ToList();
 
         Console.WriteLine(Task1(input));
-        Console.WriteLine(Task2());
+        Console.WriteLine(Task2(input));
     }
 
     public static int Task1(List<string> input)
@@ -21,7 +21,61 @@ public static class Program
         var tail = (x: 0, y: 0);
         set.UpdateSet(tail);
         var head = (x: 0, y: 0);
+        var movements = GetMovements(input);
+
+        foreach (var move in movements)
+        {
+            head.ApplyMove(move);
+            var tailMove = GetDirectionDistance(head, tail);
+            tail.ApplyMove(tailMove);
+            set.UpdateSet(tail);
+        }
+
+        return set.Count;
+    }
+
+
+    public static int Task2(List<string> input)
+    {
+        var set = new HashSet<int>();
+        var knots = new List<(int x, int y)>
+        {
+            (x: 0, y: 0),
+            (x: 0, y: 0),
+            (x: 0, y: 0),
+            (x: 0, y: 0),
+            (x: 0, y: 0),
+            (x: 0, y: 0),
+            (x: 0, y: 0),
+            (x: 0, y: 0),
+            (x: 0, y: 0),
+            (x: 0, y: 0),
+        };
+        set.UpdateSet(knots[9]);
+        var movements = GetMovements(input);
+
+        foreach (var move in movements)
+        {
+            var head = knots[0];
+            head.ApplyMove(move);
+            knots[0] = head;
+            for (int i = 1; i < knots.Count; i++)
+            {
+                var cur = knots[i];
+                var tailMove = GetDirectionDistance(knots[i - 1], cur);
+                cur.ApplyMove(tailMove);
+                if (i == 9) set.UpdateSet(cur);
+                knots[i] = cur;
+            }
+        }
+
+        return set.Count;
+    }
+
+    public static List<(int x, int y)> GetMovements(List<string> input)
+    {
         var movements = new List<(int x, int y)>();
+
         foreach (var row in input)
         {
             var split = row.Split(' ');
@@ -40,26 +94,17 @@ public static class Program
             }
         }
 
-        for (int i = 0; i < movements.Count; i++)
-        {
-            var cur = movements[i];
-            head.ApplyMove(cur);
-            var tailMove = GetDirectionDistance(head, tail);
-            tail.ApplyMove(tailMove);
-            set.UpdateSet(tail);
-        }
-
-        return set.Count;
-
+        return movements;
     }
-    public static string Task2() { return ""; }
 
     public static void UpdateSet(this HashSet<int> hSet, (int, int) point) => hSet.Add(HashCode.Combine(point.Item1, point.Item2));
+
     public static void ApplyMove(this ref (int, int) point, (int, int) move)
     {
         point.Item1 += move.Item1;
         point.Item2 += move.Item2;
     }
+
     public static (int, int) GetDirectionDistance(this (int, int) head, (int, int) tail)
     {
         var x = head.Item1 - tail.Item1;
--- 2022/Day_09/test/Tests.cs
+++ 2022/Day_09/test/Tests.cs
@@ -8,6 +8,7 @@ namespace Day_09.Test;
 public class Test
 {
     List<string> input;
+    List<string> input2;
     public Test()
     {
         input = new List<string>
@@ -21,6 +22,16 @@ public class Test
             "L 5",
             "R 2"
         };
+        input2 = new List<string>{
+            "R 5",
+            "U 8",
+            "L 8",
+            "D 3",
+            "R 17",
+            "D 10",
+            "L 25",
+            "U 20",
+        };
     }
 
     [Fact]
@@ -32,6 +43,12 @@ public class Test
     [Fact]
     public void Task2_1()
     {
-        Assert.Equal("", Program.Task2());
+        Assert.Equal(1, Program.Task2(input));
+    }
+
+    [Fact]
+    public void Task2_2()
+    {
+        Assert.Equal(36, Program.Task2(input2));
     }
 }
