c089f05976f7a79b0928d4d7846e50e8b0a0d549
Dgaduin <dgaduin@gmail.com>
2022-12-20 13:03:19 +0000

    22 D14 T1

---
+++ 2022/Day_14/src/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Day_14;
+public static class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> input = File.ReadLines("input.txt").ToList();
+        // string input = File.ReadAllText("input.txt");
+        var map = CreateMap(input);
+        Console.WriteLine(Task1(map));
+        Console.WriteLine(Task2());
+    }
+
+    public static int Task1(HashSet<(int x, int y)> map)
+    {
+        var lowerBound = map.MaxBy(a => a.y).y;
+        var startingPoint = (x: 500, y: 0);
+        var sand = startingPoint;
+        var counter = 0;
+        while (true)
+        {
+            if (sand.y >= lowerBound)
+                break;
+
+            if (!map.Contains((sand.x, sand.y + 1)))
+                sand = (sand.x, sand.y + 1);
+            else if (!map.Contains((sand.x - 1, sand.y + 1)))
+                sand = (sand.x - 1, sand.y + 1);
+            else if (!map.Contains((sand.x + 1, sand.y + 1)))
+                sand = (sand.x + 1, sand.y + 1);
+            else
+            {
+                map.Add(sand);
+                counter++;
+                sand = startingPoint;
+            }
+        }
+        return counter;
+    }
+    public static string Task2() { return ""; }
+
+    public static HashSet<(int x, int y)> CreateMap(List<string> input)
+    {
+        var map = new HashSet<(int x, int y)>();
+
+        var parsedInput = input.Select(x =>
+            x.Split(" -> ")
+             .Select(y =>
+             {
+                 var t = y.Split(',');
+                 return (x: Int32.Parse(t[0]), y: Int32.Parse(t[1]));
+             })
+             .ToList());
+
+        foreach (var line in parsedInput)
+        {
+            var previousPoint = line[0];
+            for (int i = 1; i < line.Count; i++)
+            {
+                var nextPoint = line[i];
+                if (nextPoint.x == previousPoint.x)
+                {
+                    var min = Math.Min(previousPoint.y, nextPoint.y);
+                    var max = Math.Max(previousPoint.y, nextPoint.y);
+                    var t = Enumerable.Range(min, max - min).Select(x => (x: previousPoint.x, y: x));
+                    map.UnionWith(t);
+                }
+                else
+                {
+                    var min = Math.Min(previousPoint.x, nextPoint.x);
+                    var max = Math.Max(previousPoint.x, nextPoint.x);
+                    var t = Enumerable.Range(min, max - min + 1).Select(x => (x: x, y: previousPoint.y));
+                    map.UnionWith(t);
+                }
+                previousPoint = nextPoint;
+            }
+        }
+
+        return map;
+    }
+}
