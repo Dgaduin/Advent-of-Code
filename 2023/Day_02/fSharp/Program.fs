2fb28fce36eb9d9d8f9cdd51d045d23794912578
Atanas Pashkov <dgaduin@gmail.com>
2023-12-02 21:37:52 +0000

    Day 2 F#

---
+++ 2023/Day_02/fSharp/Program.fs
@@ -0,0 +1,54 @@
+﻿open System
+open System.IO
+
+let input = File.ReadAllLines "../src/input.txt" |> List.ofArray
+
+type Line = { Id: int; Draws: Map<string, int> }
+
+let extractDraws (s: string) =
+    let mapDraws (draws: string[]) =
+        let mapSingleDraw (draw: string) =
+            draw.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            |> fun x -> (x[1], x[0] |> Int32.Parse)
+
+        Array.map mapSingleDraw draws
+
+    s.Split ',' |> mapDraws
+
+let extractLine (line: string) =
+    let splitOptions =
+        StringSplitOptions.RemoveEmptyEntries ||| StringSplitOptions.TrimEntries
+
+    let split = line.Split([| ':'; ';' |], splitOptions)
+
+    let id = split[0].Split(' ')[1] |> Int32.Parse
+
+    let draws =
+        Array.map extractDraws split[1..]
+        |> Array.collect (fun x -> x)
+        |> Array.groupBy (fun (key, _) -> key)
+        |> Array.map (fun (key, values) -> (key, Array.maxBy snd values |> snd))
+        |> Map.ofArray
+
+    { Id = id; Draws = draws }
+
+let checkIfPossible (line: Line) =
+    if line.Draws["green"] > 13 then false
+    else if line.Draws["blue"] > 14 then false
+    else if line.Draws["red"] > 12 then false
+    else true
+
+let lines = List.map extractLine input
+
+//Task 1
+lines
+|> List.filter checkIfPossible
+|> List.sumBy (fun x -> x.Id)
+|> printfn "%d"
+
+//Task 2
+
+let getPower (line: Line) =
+    line.Draws["green"] * line.Draws["red"] * line.Draws["blue"]
+
+lines |> List.map getPower |> List.sum |> printfn "%d"
